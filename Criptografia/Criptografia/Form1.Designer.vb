5a68e05befc6f015fc1949dde5ec3e333bed5130
Luis Fernando <crynando@outlook.com>
2016-04-11 21:59:21 -0300

    Cryptography
    Converter Strings and Files to MD5 or SHA-1

---
+++ Criptografia/Criptografia/Form1.Designer.vb
@@ -0,0 +1,211 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.tbString = New System.Windows.Forms.TextBox()
+        Me.btnGenerate = New System.Windows.Forms.Button()
+        Me.rbSHA1 = New System.Windows.Forms.RadioButton()
+        Me.rbMD5 = New System.Windows.Forms.RadioButton()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.btnSearch = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.tbSearch = New System.Windows.Forms.TextBox()
+        Me.tbResult = New System.Windows.Forms.TextBox()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'tbString
+        '
+        Me.tbString.Location = New System.Drawing.Point(20, 21)
+        Me.tbString.Multiline = True
+        Me.tbString.Name = "tbString"
+        Me.tbString.Size = New System.Drawing.Size(379, 81)
+        Me.tbString.TabIndex = 0
+        '
+        'btnGenerate
+        '
+        Me.btnGenerate.Location = New System.Drawing.Point(431, 89)
+        Me.btnGenerate.Name = "btnGenerate"
+        Me.btnGenerate.Size = New System.Drawing.Size(75, 23)
+        Me.btnGenerate.TabIndex = 2
+        Me.btnGenerate.Text = "Generate"
+        Me.btnGenerate.UseVisualStyleBackColor = True
+        '
+        'rbSHA1
+        '
+        Me.rbSHA1.AutoSize = True
+        Me.rbSHA1.Location = New System.Drawing.Point(405, 43)
+        Me.rbSHA1.Name = "rbSHA1"
+        Me.rbSHA1.Size = New System.Drawing.Size(53, 17)
+        Me.rbSHA1.TabIndex = 3
+        Me.rbSHA1.TabStop = True
+        Me.rbSHA1.Text = "SHA1"
+        Me.rbSHA1.UseVisualStyleBackColor = True
+        '
+        'rbMD5
+        '
+        Me.rbMD5.AutoSize = True
+        Me.rbMD5.Location = New System.Drawing.Point(405, 66)
+        Me.rbMD5.Name = "rbMD5"
+        Me.rbMD5.Size = New System.Drawing.Size(48, 17)
+        Me.rbMD5.TabIndex = 4
+        Me.rbMD5.TabStop = True
+        Me.rbMD5.Text = "MD5"
+        Me.rbMD5.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(-187, 175)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "f"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'btnSearch
+        '
+        Me.btnSearch.Location = New System.Drawing.Point(431, 34)
+        Me.btnSearch.Name = "btnSearch"
+        Me.btnSearch.Size = New System.Drawing.Size(75, 23)
+        Me.btnSearch.TabIndex = 7
+        Me.btnSearch.Text = "File Search"
+        Me.btnSearch.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.tbString)
+        Me.GroupBox1.Controls.Add(Me.rbSHA1)
+        Me.GroupBox1.Controls.Add(Me.rbMD5)
+        Me.GroupBox1.Controls.Add(Me.btnGenerate)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(551, 129)
+        Me.GroupBox1.TabIndex = 8
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Generate Hash String"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(405, 24)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(31, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Type"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.tbSearch)
+        Me.GroupBox2.Controls.Add(Me.btnSearch)
+        Me.GroupBox2.Location = New System.Drawing.Point(12, 147)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(551, 73)
+        Me.GroupBox2.TabIndex = 9
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Generate Hash File"
+        '
+        'tbSearch
+        '
+        Me.tbSearch.Location = New System.Drawing.Point(20, 34)
+        Me.tbSearch.Name = "tbSearch"
+        Me.tbSearch.Size = New System.Drawing.Size(379, 20)
+        Me.tbSearch.TabIndex = 8
+        '
+        'tbResult
+        '
+        Me.tbResult.Location = New System.Drawing.Point(20, 19)
+        Me.tbResult.Name = "tbResult"
+        Me.tbResult.Size = New System.Drawing.Size(515, 20)
+        Me.tbResult.TabIndex = 10
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.tbResult)
+        Me.GroupBox3.Location = New System.Drawing.Point(12, 238)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(551, 53)
+        Me.GroupBox3.TabIndex = 11
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Result"
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(443, 297)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 12
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(579, 326)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "N GENERATE HASH V0.1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents tbString As System.Windows.Forms.TextBox
+    Friend WithEvents btnGenerate As System.Windows.Forms.Button
+    Friend WithEvents rbSHA1 As System.Windows.Forms.RadioButton
+    Friend WithEvents rbMD5 As System.Windows.Forms.RadioButton
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents btnSearch As System.Windows.Forms.Button
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents tbSearch As System.Windows.Forms.TextBox
+    Friend WithEvents tbResult As System.Windows.Forms.TextBox
+    Friend WithEvents GroupBox3 As System.Windows.Forms.GroupBox
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+
+End Class
---
+++ Criptografia/Criptografia/Form1.vb
@@ -0,0 +1,79 @@
+﻿Imports System.Security.Cryptography
+Imports System.IO
+Imports System.Text
+
+Public Class Form1
+
+    Function gerarSHA1(palavra As String) As String
+        Dim fornece As New SHA1CryptoServiceProvider 
+        Dim byteHash() As Byte
+        Dim hash As String = ""
+
+        byteHash = fornece.ComputeHash(Encoding.UTF8.GetBytes(palavra))
+        fornece.Clear()
+        hash = BitConverter.ToString(byteHash).Replace("-", String.Empty)
+
+        Return hash.ToLower
+    End Function
+
+    Function gerarMD5(palavra As String) As String
+        Dim fornece As New MD5CryptoServiceProvider
+        Dim byteHash() As Byte
+        Dim hash As String = ""
+
+        byteHash = fornece.ComputeHash(Encoding.UTF8.GetBytes(palavra))
+        fornece.Clear()
+        hash = BitConverter.ToString(byteHash).Replace("-", String.Empty)
+
+        Return hash.ToLower
+
+    End Function
+
+    Function gerarHashArq(caminhoArquivo As String) As String
+        Dim fornece As New MD5CryptoServiceProvider
+        Dim byteHash() As Byte
+        Dim hash As String = ""
+        Dim arquivoStream As FileStream = File.OpenRead(caminhoArquivo)
+        arquivoStream.Position = 0
+
+        byteHash = fornece.ComputeHash(arquivoStream)
+        hash = BitConverter.ToString(byteHash).Replace("-", String.Empty)
+
+        Return hash.ToLower
+
+    End Function
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnGenerate.Click
+        If (tbString.Text <> "") Then
+            If (rbSHA1.Checked) Then
+                tbResult.Text = gerarSHA1(tbString.Text)
+            ElseIf (rbMD5.Checked) Then
+                tbResult.Text = gerarMD5(tbString.Text)
+            Else
+                MsgBox("Escolha algum tipo")
+            End If
+        Else
+            MsgBox("Preencha o campo")
+        End If
+
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles btnSearch.Click
+        Dim hash As String = ""
+        If (OpenFileDialog1.ShowDialog = Windows.Forms.DialogResult.OK) Then
+            hash = gerarHashArq(OpenFileDialog1.FileName)
+        End If
+        tbSearch.Text = OpenFileDialog1.FileName
+        tbResult.Text = hash
+    End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles btnClear.Click
+        tbResult.Clear()
+        tbSearch.Clear()
+        tbString.Clear()
+        rbMD5.Checked = False
+        rbSHA1.Checked = False
+    End Sub
+End Class
+
